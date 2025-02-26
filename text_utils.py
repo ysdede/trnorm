b87880b5ee86c5d269444fcba2ea51f2cdaaffe1
ysdede <yunusseyhandede@gmail.com>
2025-02-26 15:23:57 +0300

    feat: ekle

--- text_utils.py
+++ text_utils.py
@@ -64,4 +64,38 @@ def turkish_capitalize(s):
     if not s:
         return s
     return turkish_upper(s[0]) + s[1:]
+
+def ekle(kelime: str = "", ek: str = ""):
+    ekler = ["ile", "ise"]
+    if len(kelime) == 0:
+        return ""
+    if ek == "" or ek not in ekler:
+        raise ValueError(f"Ek {ek} not in {ekler}")
+
+    from istisnalar import ek_istisnalar_unlu_uyumu, suffix_tuple
+
+    # Beklenen girdi tek kelime ancak bazı durumlarda birden fazla kelime verilebilir
+    kelime = kelime.split(" ")[-1]
+
+    # Kelimede sesli harf yoksa ek oluşturulamaz, "kelime + \s + ile"
+    if not any(turkish_lower(harf) in sesli_harfler for harf in kelime):
+        return f"{kelime} ile"
     
+    duz_kucuk_kelime = sapkasiz(turkish_lower(kelime))
+
+    if duz_kucuk_kelime in ek_istisnalar_unlu_uyumu.keys():
+        return f"{kelime}{ek_istisnalar_unlu_uyumu[duz_kucuk_kelime][1]}"
+
+    yeni_ek = ""
+
+    if sesli_ile_bitiyor(kelime):
+        yeni_ek = yeni_ek + "y"
+
+    yeni_ek = yeni_ek + ek[1]
+
+    if son_sesli_harf_kalin(kelime):
+        yeni_ek = yeni_ek + "a"
+    else:
+        yeni_ek = yeni_ek + "e"
+
+    return f"{kelime}{yeni_ek}"
